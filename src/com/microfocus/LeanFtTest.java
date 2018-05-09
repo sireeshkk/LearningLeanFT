da6e00b0dd9ee3a1c6794401754d815f23dedcc4
sercely <ron.sercely@hpe.com>
2018-05-09 14:33:57 -0500

    Initial Commit

---
+++ src/com/microfocus/LeanFtTest.java
@@ -0,0 +1,47 @@
+package com.microfocus;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import com.hp.lft.sdk.web.*;
+
+import unittesting.*;
+
+public class LeanFtTest extends UnitTestClassBase {
+
+    public LeanFtTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new LeanFtTest();
+        globalSetup(LeanFtTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+        Browser browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.navigate("http://www.advantageonlineshopping.com");
+    }
+
+}
