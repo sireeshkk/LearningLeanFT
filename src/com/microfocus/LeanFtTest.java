e16b3f1ba95f9f953cf268e2fe087ffa995007f8
sercely <ron.sercely@hpe.com>
2018-05-11 14:12:50 -0500

    Using maven to data drive  test

--- src/com/microfocus/LeanFtTest.java
+++ src/com/microfocus/LeanFtTest.java
@@ -1,91 +1,78 @@
-package com.microfocus;
+package somename;
 
 import static org.junit.Assert.*;
 
-import com.hp.lft.report.ReportException;
-import com.hp.lft.report.Reporter;
-import com.hp.lft.report.Status;
-import com.hp.lft.unittesting.Parameters;
+import com.hp.lft.sdk.web.Browser;
+import com.hp.lft.sdk.web.BrowserFactory;
+import com.hp.lft.sdk.web.BrowserType;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import com.hp.lft.sdk.*;
-import com.hp.lft.verifications.*;
 import com.hp.lft.sdk.web.*;
+import com.hp.lft.verifications.*;
+import org.apache.commons.csv.*;
 
 import unittesting.*;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class LeanFtTest extends UnitTestClassBase {
 
-    public LeanFtTest() throws GeneralLeanFtException, ReportException {
+    public LeanFtTest() {
         //Change this constructor to private if you supply your own public constructor
     }
 
+    Browser browser;
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
+    public static  void setUpBeforeClass() throws Exception {
         instance = new LeanFtTest();
         globalSetup(LeanFtTest.class);
     }
 
     @AfterClass
-    public static void tearDownAfterClass() throws Exception {
+    public  static void tearDownAfterClass() throws Exception {
         globalTearDown();
     }
 
     @Before
     public void setUp() throws Exception {
+        browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.navigate("http://advantageonlineshopping.com:8080");
     }
 
     @After
     public void tearDown() throws Exception {
-    }
-
-
-    @Test
-    public void testChrome() throws GeneralLeanFtException,ReportException
-    {
-        test(BrowserType.CHROME);
-    }
-
-    @Test
-    public void testFirefox() throws GeneralLeanFtException,ReportException
-    {
-        test(BrowserType.FIREFOX);
-    }
-
-    @Parameters({"browserName"})
-    public void test(BrowserType browserName) throws GeneralLeanFtException, ReportException {
-        Browser browser = BrowserFactory.launch(browserName);
-        // was Browser browser = BrowserFactory.launch(BrowserType.CHROME);
-
-        browser.navigate("http://advantageonlineshopping.com");
-
-        LearnApplicationModels appModel = new LearnApplicationModels(browser);
-
-        appModel.SPEAKERSLink().click();
-        appModel.BuyNow().click();
-        appModel.SaveToCartButton().click();
-        appModel.MenuCartWebElement().click();
-        String total = appModel.TotalInCart().getInnerText();
-        String[] total_split = total.split("\\$");
-        String justNumber = total_split[1];
-        System.out.println(justNumber);
-        String noCommas = justNumber.replaceAll(",","");
-        System.out.println(noCommas);
-        double totalValue = Double.valueOf(noCommas);
-        Verify.greaterOrEqual(totalValue, 2000.0, "VerName", "Long verification description");
-
-        if (totalValue >= 2000) {
-            Reporter.reportEvent("alsoVerName", "In this description - it passed", Status.Passed);
-        } else {
-            Reporter.reportEvent("alsoVerName", "In this description - something wrong, failed!", Status.Failed);
-        }
-        appModel.REMOVEWebElement().click();
-        appModel.HOMELink().click();
-
         browser.close();
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException, IOException {
+        Reader reader = Files.newBufferedReader(Paths.get("./resources/search.csv"));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+
+        for (CSVRecord csvRecord : csvRecords) {
+            // Accessing Values by Column Index
+
+            String search = csvRecord.get(0);
+            System.out.println(search);
+
+            browser.describe(Link.class, new LinkDescription.Builder()
+                    .innerText(search)
+                    .tagName("SPAN").build()).click();
+            browser.back();
+        }
+        csvParser.close();
+        reader.close();
+
+
     }
 
 }
