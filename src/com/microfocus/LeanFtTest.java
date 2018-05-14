639324751144f6fce283599f85ea06f4833246c6
sercely <ron.sercely@hpe.com>
2018-05-14 12:24:35 -0500

    Using maven to data drive  test

--- src/com/microfocus/LeanFtTest.java
+++ src/com/microfocus/LeanFtTest.java
@@ -1,78 +1,47 @@
-package somename;
+package com.microfocus;
 
-import static org.junit.Assert.*;
-
-import com.hp.lft.sdk.web.Browser;
-import com.hp.lft.sdk.web.BrowserFactory;
-import com.hp.lft.sdk.web.BrowserType;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import javafx.util.converter.LocalTimeStringConverter;
+import org.testng.annotations.*;
 import com.hp.lft.sdk.*;
 import com.hp.lft.sdk.web.*;
 import com.hp.lft.verifications.*;
-import org.apache.commons.csv.*;
-
+import java.time.*;
 import unittesting.*;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class LeanFtTest extends UnitTestClassBase {
 
-    public LeanFtTest() {
-        //Change this constructor to private if you supply your own public constructor
-    }
-
-    Browser browser;
     @BeforeClass
-    public static  void setUpBeforeClass() throws Exception {
-        instance = new LeanFtTest();
-        globalSetup(LeanFtTest.class);
+    public void beforeClass() throws Exception {
     }
 
     @AfterClass
-    public  static void tearDownAfterClass() throws Exception {
-        globalTearDown();
+    public void afterClass() throws Exception {
     }
 
-    @Before
-    public void setUp() throws Exception {
-        browser = BrowserFactory.launch(BrowserType.CHROME);
-        browser.navigate("http://advantageonlineshopping.com:8080");
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
     }
 
-    @After
-    public void tearDown() throws Exception {
-        browser.close();
+    @AfterMethod
+    public void afterMethod() throws Exception {
+
     }
+//    BrowserType bType = BrowserType.CHROME; //    @Parameters({"browserToOpen"})
 
     @Test
-    public void test() throws GeneralLeanFtException, IOException {
-        Reader reader = Files.newBufferedReader(Paths.get("./resources/search.csv"));
-        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-        Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+    @Parameters({"browserToOpen"})
+    public void myNewTest(BrowserType bType, String printMe) throws GeneralLeanFtException, InterruptedException {
+//        System.out.println("Starting" + printMe  + ": " +LocalTime.now());
+        Browser browser = BrowserFactory.launch(bType);
+        Thread.sleep(3+1000);
+        browser.navigate("msn.com");
+        System.out.println(printMe);
+        Thread.sleep(5*1000);
+        browser.navigate("google.com");
+        Thread.sleep(5*1000);
+//        System.out.println("Stopping:" + LocalTime.now());
 
-        for (CSVRecord csvRecord : csvRecords) {
-            // Accessing Values by Column Index
-
-            String search = csvRecord.get(0);
-            System.out.println(search);
-
-            browser.describe(Link.class, new LinkDescription.Builder()
-                    .innerText(search)
-                    .tagName("SPAN").build()).click();
-            browser.back();
-        }
-        csvParser.close();
-        reader.close();
-
-
+        browser.close();
     }
 
 }
